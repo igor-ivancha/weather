df1bdce8af4aa64e890a1fb3e340207e3d08e34a
igor-ivancha <sg.igor.iv@gmail.com>
2017-03-29 16:12:22 +0300

    add axios and redux-promises, implement middleware, work with weather api

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'eeeb74790177bc6686c4313ca86acec5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
